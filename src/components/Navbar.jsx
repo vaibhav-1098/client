105ac4953abb5191b75b1d3dcd88bae67b1e7d54
vaibhav-1098 <vaibhav.2003.rana@gmail.com>
2024-10-07 18:37:52 +0530

    _

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="flex justify-center items-center bg-violet-600 text-white p-2 gap-5">
+        <nav className="flex justify-center items-center bg-blue-600 text-white p-2 gap-5">
             <NavLink to="/" style={({ isActive }) => (isActive ? activeStyling : {})}>
                 Home
             </NavLink>
